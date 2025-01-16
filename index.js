b11023b6ff446fa73887c51b480f2bff31fd4389
Apk <111860693+MrApk@users.noreply.github.com>
2025-01-16 20:56:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,123 +1,18 @@
 const mineflayer = require('mineflayer')
-const pvp = require('mineflayer-pvp').plugin
-const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
-const armorManager = require('mineflayer-armor-manager')
-const keep_alive = require('./keep_alive.js')
 
 const bot = mineflayer.createBot({
     host: 'SakshamNehra.aternos.me',
     port: 53763,
     username: 'APKbot',
     password: '',
-    logErrors: false
-})
-
-bot.loadPlugin(pvp)
-bot.loadPlugin(armorManager)
-bot.loadPlugin(pathfinder)
-
-bot.on('playerCollect', (collector, itemDrop) => {
-    if (collector !== bot.entity) return
-
-    setTimeout(() => {
-        const sword = bot.inventory.items().find(item => item.name.includes('sword'))
-        if (sword) bot.equip(sword, 'hand')
-    }, 150)
-})
-
-bot.on('playerCollect', (collector, itemDrop) => {
-    if (collector !== bot.entity) return
-
-    setTimeout(() => {
-        const shield = bot.inventory.items().find(item => item.name.includes('shield'))
-        if (shield) bot.equip(shield, 'off-hand')
-    }, 250)
-})
-
-let guardPos = null
-
-function guardArea(pos) {
-    guardPos = pos.clone()
-
-    if (!bot.pvp.target) {
-        moveToGuardPos()
-    }
-}
-
-function stopGuarding() {
-    guardPos = null
-    bot.pvp.stop()
-    bot.pathfinder.setGoal(null)
-}
-
-function moveToGuardPos() {
-    const mcData = require('minecraft-data')(bot.version)
-    bot.pathfinder.setMovements(new Movements(bot, mcData))
-    bot.pathfinder.setGoal(new goals.GoalBlock(guardPos.x, guardPos.y, guardPos.z))
-}
-
-bot.on('stoppedAttacking', () => {
-    if (guardPos) {
-        moveToGuardPos()
-    }
-})
-
-bot.on('physicsTick', () => {
-    if (bot.pvp.target) return
-    if (bot.pathfinder.isMoving()) return
-
-    const entity = bot.nearestEntity()
-    if (entity) bot.lookAt(entity.position.offset(0, entity.height, 0))
-})
-
-bot.on('physicsTick', () => {
-    if (!guardPos) return
-
-    const filter = e => e.type === 'mob' && e.position.distanceTo(bot.entity.position) < 16 &&
-        e.mobType !== 'Armor Stand' // Mojang classifies armor stands as mobs for some reason?
-
-    const entity = bot.nearestEntity(filter)
-    if (entity) {
-        bot.pvp.attack(entity)
-    }
+    logErrors: false     // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
 })
 
 bot.on('chat', (username, message) => {
-    if (message === 'guard') {
-        const player = bot.players[username]
-
-        if (!player) {
-            bot.chat("I can't see you.")
-            return
-        }
-
-        bot.chat('I will guard that location.')
-        guardArea(player.entity.position)
-    }
-
-    if (message === 'fight me') {
-        const player = bot.players[username]
-
-        if (!player) {
-            bot.chat("I can't see you.")
-            return
-        }
-
-        bot.chat('Prepare to fight!')
-        bot.pvp.attack(player.entity)
-    }
-
-    if (message === 'stop') {
-        bot.chat('I will no longer guard this area.')
-        stopGuarding()
-    }
+  if (username === bot.username) return
+  bot.chat(message)
 })
 
-// Error Handling
-bot.on('error', (err) => {
-    console.error('Error:', err.message)
-})
-
-bot.on('kicked', (reason) => {
-    console.error('Kicked:', reason)
-})
+// Log errors and kick reasons:
+bot.on('kicked', console.log)
+bot.on('error', console.log)
